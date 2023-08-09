99d5dc459210431b472f98faaa78aaf64ac6f7d8
trongvn@vng.com.vn <trongvn@vng.com.vn>
2023-08-10 00:51:28 +0700

    Complete useYjs

---
+++ src/YConnectionManager.js
@@ -0,0 +1,66 @@
+import * as Y from "yjs";
+import { WebsocketProvider } from "y-websocket";
+
+class YConnectionManager {
+  constructor() {
+    this.yConnectionMap = {};
+    window.addEventListener("beforeunload", () => {
+      Object.keys(this.yConnectionMap).forEach((signaling) => {
+        Object.keys(this.yConnectionMap[signaling]).forEach((roomName) => {
+          this._destroyYdoc(signaling, roomName);
+        });
+      });
+    });
+  }
+
+  getYConnection(signaling, roomName) {
+    if (!this.yConnectionMap[signaling]) {
+      this.yConnectionMap[signaling] = {};
+    }
+    if (!this.yConnectionMap[signaling][roomName]) {
+      let yDoc = new Y.Doc();
+      let provider = new WebsocketProvider(signaling, roomName, yDoc, {
+        disableBc: true,
+      });
+      this.yConnectionMap[signaling][roomName] = {
+        yDoc,
+        provider,
+      };
+    }
+    return this.yConnectionMap[signaling][roomName];
+  }
+
+  leaveRoom(signaling, roomName) {
+    if (
+      !!this.yConnectionMap[signaling] &&
+      !!this.yConnectionMap[signaling][roomName]
+    ) {
+      this._destroyYdoc(signaling, roomName);
+      delete this.yConnectionMap[signaling][roomName];
+      if (Object.keys(this.yConnectionMap[signaling]).length === 0) {
+        delete this.yConnectionMap[signaling];
+      }
+    }
+  }
+
+  _destroyYdoc(signaling, roomName) {
+    if (
+      this.yConnectionMap[signaling][
+        roomName
+      ].provider.awareness.getStates().size === 1
+    ) {
+      for (let key of this.yConnectionMap[signaling][roomName].yDoc
+        .getMap()
+        .keys()) {
+        this.yConnectionMap[signaling][roomName].yDoc
+          .getMap()
+          .delete(key);
+      }
+      this.yConnectionMap[signaling][roomName].yDoc.destroy();
+    }
+  }
+}
+
+const Y_CONNECTION_MANAGER = new YConnectionManager();
+
+export default Y_CONNECTION_MANAGER;
---
+++ src/useYjs.js
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import * as Y from "yjs";
+import Y_CONNECTION_MANAGER from "./YConnectionManager";
+
+export default function useYjs({
+  signaling,
+  roomName,
+  initData = {},
+  initUserInfo = {},
+  intervalSyncTime = 250,
+}) {
+  const [ydoc] = useState(
+    Y_CONNECTION_MANAGER.getYConnection(signaling, roomName).yDoc
+  );
+  const [data, setData] = useState({
+    ...initData,
+    ...Object.fromEntries(new Map(ydoc.getMap().entries())),
+  });
+  const [provider] = useState(
+    Y_CONNECTION_MANAGER.getYConnection(signaling, roomName).provider
+  );
+
+  const [userInfoMap, setUserInfoMap] = useState({});
+
+  const [canSyncAwareness, setCanSyncAwareness] = useState(false);
+
+  useEffect(() => {
+    return () => leaveRoom(signaling, roomName);
+  }, [signaling, roomName])
+
+  useEffect(() => {
+    let syncInterval = setInterval(() => {
+      setCanSyncAwareness(true);
+    }, intervalSyncTime);
+    return () => clearInterval(syncInterval);
+  }, [intervalSyncTime]);
+
+  useEffect(() => {
+    if (!ydoc.getMap()) {
+      Object.keys(initData).forEach((key) => {
+        ydoc.getMap().set(key, initData[key]);
+      });
+    }
+  }, [ydoc]);
+
+  useEffect(() => {
+    setData((prev) => ({
+      ...prev,
+      ...Object.fromEntries(new Map(ydoc.getMap().entries())),
+    }));
+  }, [ydoc]);
+
+  useEffect(() => {
+    ydoc.on("update", (update) => {
+      Y.applyUpdate(ydoc, update);
+      setData(Object.fromEntries(new Map(ydoc.getMap().entries())));
+    });
+  }, [ydoc]);
+
+  useEffect(() => {
+    provider.awareness.setLocalState(initUserInfo);
+  }, [provider]);
+
+  useEffect(() => {
+    provider.awareness.on("change", (changes) => {
+      provider._awarenessUpdateHandler(changes);
+      setUserInfoMap(Object.fromEntries(provider.awareness.getStates()));
+    });
+  }, [provider]);
+
+  const updateData = (newData) => {
+    Object.keys(newData).forEach((key) => {
+      if (newData[key] !== data[key]) {
+        ydoc.getMap().set(key, newData[key]);
+      }
+    });
+  };
+
+  const updateFieldData = (fieldName, value) => {
+    ydoc.getMap().set(fieldName, value);
+  };
+
+  const updateLocalUserInfo = (userInfo) => {
+    if (canSyncAwareness) {
+      provider.awareness.setLocalState(userInfo);
+      setCanSyncAwareness(false)
+    }
+  };
+
+  const updateFieldLocalUserInfo = (fieldName, value) => {
+    if (canSyncAwareness) {
+      provider.awareness.setLocalStateField(fieldName, value);
+      setCanSyncAwareness(false)
+    }
+  };
+
+  const leaveRoom = (signaling, roomName) => {
+    Y_CONNECTION_MANAGER.leaveRoom(signaling, roomName)
+  }
+
+  return {
+    data: data,
+    updateData,
+    updateFieldData,
+    localID: provider.awareness.clientID.toString(),
+    localUserInfo: provider?.awareness?.getLocalState(),
+    userInfoMap,
+    updateLocalUserInfo,
+    updateFieldLocalUserInfo,
+  };
+}
